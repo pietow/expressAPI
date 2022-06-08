5d35144a67935e813291c9a6290b2f8a8d168dd8
piet <pprotassow@posteo.de>
2022-06-08 13:22:37 +0200

    feat: post book and append posted book to book list

--- app.js
+++ app.js
@@ -5,11 +5,16 @@ const app = express()
 const port = 3000
 const books = require('./techBooks.json') //Load data into memory; To make data persitent, you usally use a Database e.g. MongoDB
 
-app.get('/', (req, res) => {
+app.use(express.urlencoded({ extended: true })) //If extended is false, you can not post "nested object"
+
+app.get('/book', (req, res) => {
+    console.log(books.length)
     res.json(books)
 })
 
-app.post('/', (req, res) => {
+app.post('/book', (req, res) => {
+    const book = Object.assign({ id: books.length + 1 }, req.body)
+    books.push(book)
     res.send('Got a POST request')
 })
 
