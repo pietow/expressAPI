d2b370181d0d1598f93df1976f547f7bf3a0ae0e
piet <pprotassow@posteo.de>
2022-06-11 15:28:58 +0200

    feat: shows users from users.json

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 /** @format */
+'use strict'
 
 const express = require('express')
 const app = express()
 const port = 3000
 const BookController = require('./controller/book.controller')
+const UserController = require('./controller/user.controller')
 
 app.use(express.urlencoded({ extended: true })) //If extended is false, you can not post "nested object"
 app.use('/book', BookController)
+app.use('/user', UserController)
 
 app.listen(port, () => {
     console.log(
--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,4 +1,5 @@
 /** @format */
+'use strict'
 
 const express = require('express')
 const router = express.Router()
@@ -6,5 +7,14 @@ const users = require('../data/users.json') //Load data into memory; To make dat
 
 //SHOW ALL USERS
 router.get('/', (req, res) => {
-    res.json(books)
+    res.json(users)
 })
+
+//SHOW ONE USER
+router.get('/:id', (req, res) => {
+    const user = users.find((user) => user.id === Number(req.params.id))
+    res.json(null) //res.json(null) --> Content-Type:application/json
+    //res.send(...) // automatically sets the Content-Type
+})
+
+module.exports = router
