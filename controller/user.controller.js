fc6b7f67dc859859285d13f67c40eecae50a06df
piet <pprotassow@posteo.de>
2022-06-11 16:44:29 +0200

    feat: login and delete function with token

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -5,6 +5,8 @@ const express = require('express')
 const router = express.Router()
 const users = require('../data/users.json') //Load data into memory; To make data persistent, you usally use a Database e.g. MongoDB
 
+const SECRET = 'verySECRET'
+
 //SHOW ALL USERS
 router.get('/', (req, res) => {
     res.json(users)
@@ -13,8 +15,40 @@ router.get('/', (req, res) => {
 //SHOW ONE USER
 router.get('/:id', (req, res) => {
     const user = users.find((user) => user.id === Number(req.params.id))
-    res.json(null) //res.json(null) --> Content-Type:application/json
+    res.json(user) //res.json(null) --> Content-Type:application/json
     //res.send(...) // automatically sets the Content-Type
+})
+
+router.post('/login', (req, res) => {
+    console.log(req.body)
+    const user = users.find(
+        (user) =>
+            user.namefirst === req.body.namefirst &&
+            user.password === req.body.password,
+    )
+    user.token = user ? SECRET : null
+    res.json(user ? user : 'Access denied!')
+})
+
+//ACCESS ROUTE ONLY WITH SECRET
+router.delete('/:id', (req, res) => {
+    const authHeader = req.headers.authorization
+    console.log(authHeader)
+
+    try {
+        const token = authHeader.split(' ')[1]
+        if (token === SECRET) {
+            const index = users.findIndex(
+                (user) => Number(user.id) === Number(req.params.id),
+            )
+            const deletedUser = users.splice(index, index === -1 ? 0 : 1)
+            res.json(deletedUser)
+        } else {
+            res.send('wrong token')
+        }
+    } catch {
+        res.send('fail')
+    }
 })
 
 module.exports = router
